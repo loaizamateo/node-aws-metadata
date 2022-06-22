d6866e1caf9f66b83a667886b80b332d95ce0a29
Mateo Loaiza <mateo_loaiza@epam.com>
2022-06-22 15:47:48 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var exec = require('child_process').exec;
+const app = express();
+
+app.get('/', (req, res) => {
+    exec('curl http://169.254.169.254/latest/meta-data/placement/region', function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+        
+        res.send(stdout);
+
+      });
+});
+
+app.listen(3000, () => {
+    console.log('App listen in port 3000')
+});
