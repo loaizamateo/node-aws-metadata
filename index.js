001222427fe8e32c29c7e19430bc43a3de0e7819
Mateo Loaiza <mateo_loaiza@epam.com>
2022-06-22 16:11:00 -0500

    change for availabilyt-zone

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ var exec = require('child_process').exec;
 const app = express();
 
 app.get('/', (req, res) => {
-    exec('curl http://169.254.169.254/latest/meta-data/placement/region', function (error, stdout, stderr) {
+    exec('curl http://169.254.169.254/latest/meta-data/placement/availability-zone', function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
           console.log('exec error: ' + error);
         }
-        
         res.send(stdout);
-
       });
 });
 
